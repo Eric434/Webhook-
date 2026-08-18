82605af5c632d17cb087b641e73a6154c52dbbe7
Eric434 <ericwalison84@gmail.com>
2026-08-18 12:19:54 -0700

    Add POST request handler to submit.js

--- api/submit.js
+++ api/submit.js
@@ -1,1 +1,14 @@
+export default function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({
+      error: "Method not allowed"
+    });
+  }
 
+  console.log("Received:", req.body);
+
+  return res.status(200).json({
+    success: true,
+    message: "Data received"
+  });
+}
